7a4f358695d1aa753f40298b1f2b269de972023f
jonr10 <jonroberts10@gmail.com>
2017-05-09 13:42:05 +0100

    basics working

---
+++ ui.R
@@ -0,0 +1,74 @@
+source(file = "global.R")
+
+##########UI.R  following the Rshiny Cheatsheet approach
+
+
+navbarPage("Email Analysis",
+               tabPanel("All Email",
+                        sidebarLayout(
+                                # Sidebar with a slider and selection inputs
+                                sidebarPanel(
+                                        selectInput("random_select", "Random order?:",
+                                                    choices = r_order$text),
+                                        sliderInput("colour_num",
+                                                    "Number of colours:",
+                                                    min = 3,  max = 8, value = 8),
+                                        sliderInput("freq",
+                                                    "Minimum Frequency:",
+                                                    min = 1,  max = 500, value = 5),
+                                        sliderInput("max",
+                                                    "Maximum Number of Words:",
+                                                    min = 1,  max = 100,  value = 50)
+                                ),
+                                mainPanel( 
+                                        plotOutput("wholeplot")
+                                )
+                        )
+               )
+               ,
+                   tabPanel("Emails to one person",
+                            sidebarLayout(
+                                    # Sidebar with a slider and selection inputs
+                                    sidebarPanel(
+                                            textInput(inputId = "Who did you email", 
+                                                      label = "Individual",
+                                                      width = '100%',
+                                                      value = "Ross Wyatt"
+                                                )
+                                            
+                                    ),
+                                    mainPanel( 
+                                            plotOutput("singleplot")
+                                    )
+                            )
+                   )
+           ,
+           
+           tabPanel("Emails to many people",
+                    sidebarLayout(
+                            # Sidebar with a slider and selection inputs
+                            sidebarPanel(
+                                    textInput(inputId = "Who did you email #1", 
+                                              label = "Individual #1",
+                                              width = '100%',
+                                              value = "Ross Wyatt"
+                                    ),
+                                    textInput(inputId = "Who did you email #2", 
+                                              label = "Individual #2",
+                                              width = '100%',
+                                              value = "Mike Marriott"
+                                    )
+                                    
+                            ),
+                            mainPanel( 
+                                    plotOutput("multiplot")
+                            )
+                    )
+                )
+       
+
+)
+
+
+
+
