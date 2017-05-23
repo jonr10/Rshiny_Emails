73c0d1cae73a354492546fd5072595ede5ce1695
jonr10 <jonroberts10@gmail.com>
2017-05-23 14:53:01 +0100

    reactive fixed and mostcommon now generic

--- global.R
+++ global.R
@@ -7,7 +7,7 @@ setwd("/Users/jonathanroberts/Documents/R/RShiny_Emails")
 
 ##Install libraries (possibly too many!)
 
-libraries <- c("tidytext", "magrittr", "dplyr", "tidyr", 
+libraries <- c("tidytext", "magrittr", "dplyr", "tidyr",
                "lazyeval", "purrr","ggplot2", "wordcloud", "reshape2", "shiny")
 
 #lapply(libraries, install.packages)  #UNCOMMENT TO INSTALL PACKAGES
@@ -15,12 +15,12 @@ lapply(libraries, library, character.only = TRUE)
 
 ##Read in email data and special stopwords, e.g. things that appear in my signature.
 
-#this is the csv that outlook spits out through it's export function. 
+#this is the csv that outlook spits out through it's export function.
 raw_data <- read.csv("../SensitiveData/2017_sent_emails.CSV",  colClasses = "character",stringsAsFactors = F)
 email_stopwords <- read.csv("../SensitiveData/email_stopwords.CSV", colClasses = "character")
 bing <- get_sentiments("bing")
 
-#rename the columns of the text and recipients so that it is easy to call later 
+#rename the columns of the text and recipients so that it is easy to call later
 colnames(raw_data)[2]<-"email"
 colnames(raw_data)[6]<-"who"
 
@@ -28,49 +28,50 @@ colnames(raw_data)[6]<-"who"
 ############USEFUL FUNCTIONS
 
 ####Make a couple of functions to unnest and tidy up the data
-#TODO:  for some reason this function doesn't allow you to pass "email" or "Subject" 
-#       as an argument and work in the way that you would expect. 
+#TODO:  for some reason this function doesn't allow you to pass "email" or "Subject"
+#       as an argument and work in the way that you would expect.
 
 tidy_stop_email <- function(text_df) {
         #manipulate the data so that each word has its own row
         tidy_Qdf<- text_df %>% unnest_tokens(word,"email",to_lower=TRUE)
         #remove stopwords
-        clean_Qdf <- tidy_Qdf %>% anti_join(stop_words)  
+        clean_Qdf <- tidy_Qdf %>% anti_join(stop_words)
         clean_Qdf <- clean_Qdf %>% anti_join(email_stopwords)
 }
 
 tidy_stop_subject <- function(text_df) {
         tidy_Qdf<- text_df %>% unnest_tokens(word,"Subject",to_lower=TRUE)
-        clean_Qdf <- tidy_Qdf %>% anti_join(stop_words)  
+        clean_Qdf <- tidy_Qdf %>% anti_join(stop_words)
         clean_Qdf <- clean_Qdf %>% anti_join(email_stopwords)
 }
 
 #####Bigrams Functions
 ## TODO: i want to be able to call to $email or $Subject in the defintion of the function, but can't do this in the obvious way.
 
-mostcommon <- function(text_df,n=1,x=20) {
+mostcommon <- function(text_df,n=1,x=20, which_text = "Subject") {
+        #manipulate the data so that each word has its own row
+        #remove stopwords
+        #count the occurrences of each word, sort by the number of occurrences, and take the top x
+
         if(n==1){
-                #manipulate the data so that each word has its own row
-                tidy_Qdf<- text_df %>% unnest_tokens(word,"Subject",to_lower=TRUE)
-                #remove stopwords
-                clean_Qdf <- tidy_Qdf %>% anti_join(stop_words)  
+                #wierd thing to solve an issue with tidytext reading in strings for variables
+                word<-"word"
+
+                # NOTE THE UNDERSCORE - solves the tidytext issue
+                tidy_Qdf<- text_df %>% unnest_tokens_(word,which_text,to_lower=TRUE)
+                clean_Qdf <- tidy_Qdf %>% anti_join(stop_words)
                 clean_Qdf <- clean_Qdf %>% anti_join(email_stopwords)
-                #count the occurrences of each word, sort by the number of occurrences, and take the top x
                 top_x <- (clean_Qdf %>% count(word,sort=TRUE))[1:x,]
         }
         else if(n==2){
-                #manipulate the data so that each bigram has its own row
-                tidy_Qdf<- text_df %>% unnest_tokens(bigram,"Subject",to_lower=TRUE,token="ngrams",n=2)
-                #separate bigrams into individual words
+                bigram<-"bigram"
+                tidy_Qdf<- text_df %>% unnest_tokens_(bigram,which_text,to_lower=TRUE,token="ngrams",n=2)
                 bigrams_separated <- tidy_Qdf %>% separate(bigram, c("word1", "word2"), sep = " ")
-                #remove cases where one of the words is a stopword
                 bigrams_filtered <- bigrams_separated %>%
                         filter(!word1 %in% stop_words$word) %>%
                         filter(!word2 %in% stop_words$word)
-                
-                #count the occurrences of word pairs, sort by the number of occurrences, and take the top x
                 top_x <- as.data.frame((bigrams_filtered %>% count(word1, word2, sort = TRUE))[1:x,])
-                
+
                 #rejoin the words back into bigrams
                 top_x$phrase <- sapply(1:x,
                                        function(x)
@@ -87,11 +88,11 @@ mostcommon <- function(text_df,n=1,x=20) {
 ######Strip out data for one person
 
 #i've design this function to only accept one name and c
-#I call it repeatedly if i want to give it lots of people to analyse at once. 
+#I call it repeatedly if i want to give it lots of people to analyse at once.
 #I guess this bit could accept a character vector.
 
 individual_emails<-function(source_data, who = "Ross"){
-        
+
         #vector to take the instances where the match is true
         who<-paste0("\\b",tolower(who),"\\b")
         v<-grepl(who, tolower(source_data$who))
@@ -107,13 +108,15 @@ individual_emails<-function(source_data, who = "Ross"){
 
 email_text<-tidy_stop_email(raw_data)
 subject_text<-tidy_stop_subject(raw_data)
-shiny_corpus<-subject_text$word
+subject_corpus<-subject_text$word
+email_corpus<-email_text$word
 commonBigrams <- raw_data %>% mostcommon(n=2)
+commonBigrams_email <- raw_data %>% mostcommon(n=2, which_text = "email")
 
 
 #######TESTBED for code before i drop it in server.ui
 ggplot(commonBigrams, aes(x = phrase, y = n, fill = who)) + geom_bar(stat = "identity", show.legend = FALSE) +
-        xlab("Terms") + ylab("Count") + coord_flip() 
+        xlab("Terms") + ylab("Count") + coord_flip()
 
 #subject_text %>%
 #        count(word) %>%
